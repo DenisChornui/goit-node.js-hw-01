7b4729bfcce857f7052ec748f37e458e02ca6849
DenisChornui <129185799+DenisChornui@users.noreply.github.com>
2024-01-09 19:52:24 +0200

    finish tasks

---
+++ README.md
@@ -0,0 +1,4 @@
+https://monosnap.com/file/ovwvMRqz2B3UhmEk0nqijBQfYWFCFU
+https://monosnap.com/file/z5AaR8zREv87DMkwsWfNyiQsPHS6Ul
+https://monosnap.com/file/Y1YRKTUSensKPqEjS5civjOG1MWNEO
+https://monosnap.com/file/py7tIbChYOsHp2lsL1ERWYnNAOCokm
---
+++ index.js
@@ -0,0 +1,46 @@
+import { program } from "commander";
+import * as contactsService from "./contacts.js";
+
+// const { Command } = require('commander');
+// const program = new Command();
+program
+  .option('-a, --action <type>', 'choose action')
+  .option('-i, --id <type>', 'user id')
+  .option('-n, --name <type>', 'user name')
+  .option('-e, --email <type>', 'user email')
+  .option('-p, --phone <type>', 'user phone');
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+
+    case "list":
+      const allContacts = await contactsService.listContacts();
+      console.table(allContacts);
+      break;
+
+    case "get":
+      const oneContact = await contactsService.getContactById(id);
+      console.log(oneContact);
+      break;
+
+    case "add":
+      const newContact = await contactsService.addContact(name, email, phone);
+      console.log(newContact);
+      break;
+
+    case "remove":
+      const deleteContact = await contactsService.removeContact(id);
+      console.log(deleteContact);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv)
